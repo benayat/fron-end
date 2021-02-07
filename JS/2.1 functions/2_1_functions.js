1fac0fc4780a9e37dc9ee426ccee87f0f674206b
Benaya Trabelsi <Benaya7@gmail.com>
2021-02-07 16:05:58 +0200

    2.2 functions

---
+++ JS/2.1 functions/2_1_functions.js
@@ -0,0 +1,6 @@
+function describeCountry(country, population, capitalCity) {
+  var str = `${country} has ${population} people and its capital city is ${capitalCity}`;
+
+  return str;
+}
+console.log(describeCountry("israel", "8 milion", "Jerusalem"));
