920175157136595598b34a5d91dc55afc5135c13
Benaya Trabelsi <Benaya7@gmail.com>
2021-02-07 14:47:00 +0200

    git add .

---
+++ JS/1.1 primitive data types/1_1.js
@@ -0,0 +1,25 @@
+console.log("it works");
+var str = 'mos mos "mos"';
+//using backtics for string is usefull because you can do this:
+// this doesn't work!
+
+str1 = `mos is
+doing his
+best`;
+
+var num = 1243.2938;
+const obj = {
+  firstName: "ben",
+  lastName: "tr",
+  age: 33,
+};
+var bool = true;
+var nothin = null;
+var notYetDefined;
+
+var sym2 = Symbol("foo");
+var arr = [str, num, obj, bool, nothin, notYetDefined];
+
+for (i = 0; i < arr.length; i++) {
+  console.log(typeof arr[i]);
+}
