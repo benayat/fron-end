369a989c1ee327af224c6505e6d32428d793b75a
Benaya Trabelsi <Benaya7@gmail.com>
2021-02-14 12:05:19 +0200

    maps vs objects: no clear winner. the winner is really the method you use to add the keys and values.

---
+++ JS/8 maps/8.2 maps vs objects/maps_vs_objects.js
@@ -0,0 +1,41 @@
+let obj1 = {};
+
+// const maxCount = 1000000;
+// console.time("My operation"); // <---- Starts the timer
+// for (let i = 0; i < maxCount; i++) {
+//   obj1[i] = { key: `value${i}` };
+// }
+// console.timeEnd("My operation"); // <---- Stops the time
+
+const myMap = new Map();
+
+const maxCount = 1000000;
+// console.time("My operation"); // <---- Starts the timer
+// for (let i = 0; i < maxCount; i++) {
+//   myMap[i] = { key: `value${i}` };
+
+// }
+// console.timeEnd("My operation"); // <---- Stops the time
+
+/* 
+not that big of a difference!
+340 for object, vs 361 of map!
+maybe the way I add it makes a difference...lets see:
+*/
+console.time("My operation"); // <---- Starts the timer
+for (let i = 0; i < maxCount; i++) {
+  myMap.set("key", i);
+  //Perform the operation to be measured multiple times
+}
+console.timeEnd("My operation"); // <---- Stops the time
+
+// and....wow!!! when I used the predefined "set" method, it simply went down 90%!!
+// only 14.59ms! now, lets check on the object if there is a better way:
+console.time("My operation"); // <---- Starts the timer
+for (let i = 0; i < maxCount; i++) {
+  obj1.i = i;
+  //Perform the operation to be measured multiple times
+}
+console.timeEnd("My operation"); // <---- Stops the time
+
+// and... object is even faster. so I think the issue here is more the way you implement and less about the collection you use.
