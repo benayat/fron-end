1cf3c30b754abf052dc689d53af87b1fc9e9aba3
Benaya Trabelsi <Benaya7@gmail.com>
2021-02-16 14:06:17 +0200

    fixing 13.4

--- JS/13/13.4/steps.js
+++ JS/13/13.4/steps.js
@@ -1,12 +1,13 @@
 function step(N) {
-  let str = "";
-
-  for (let i = 1; i < N; i++) {
-    str = "";
-    for (let j = 0; j < i; j++) {
-      str = str.concat("#");
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      if (j > i) {
+        process.stdout.write(" ");
+      } else {
+        process.stdout.write("#");
+      }
     }
-    console.log(str);
+    console.log();
   }
 }
 step(10);
