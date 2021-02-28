48b46aef87f38844f2be3be22dfcbb3f6704f4fa
Benaya Trabelsi <Benaya7@gmail.com>
2021-02-28 14:24:35 +0200

    27

---
+++ JS/27/27.2/pokeman.js
@@ -0,0 +1,24 @@
+function Pokemon(pokemonName, pokemonType, pokemonAttackList) {
+  this.name = pokemonName;
+  this.type = pokemonType;
+  this.attackList = pokemonAttackList;
+}
+
+Pokemon.prototype.callPokemon = function () {
+  console.log(`I choose you, ${this.name}`);
+};
+Pokemon.prototype.attack = function (attackNumber) {
+  console.log(`${this.name} used ${this.attackList[attackNumber]}`);
+};
+let pokemans = {
+  0: new Pokemon("Pikachu", "electric", ["static", "lightning"]),
+  1: new Pokemon("Bulbasaur", "grass", ["overgrow", "shrink"]),
+  2: new Pokemon("Charizard", "fire", ["blaze", "inferno"]),
+};
+// pokemans[0].attack(1);
+// pokemans[1].callPokemon();
+for (pokemon of Object.values(pokemans)) {
+  console.log(pokemon);
+  pokemon.attack(0);
+  pokemon.callPokemon();
+}
