5960b2880b78d0700b2f0938fdd01cbf8ddad907
Benaya Trabelsi <Benaya7@gmail.com>
2021-03-14 17:36:23 +0200

    2.2

--- react/template/src/App.js
+++ react/template/src/App.js
@@ -1,20 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+const data = ['hello', 'world'];
+const number1 = 5;
+const number2 = 6;
+const string = 'I love React!';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>“AppleSeeds React App”</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Click Me
-        </a>
+        <p>
+          {data[0].slice(0, 1).toUpperCase() +
+            data[0].slice(1) +
+            ' ' +
+            data[1].slice(0, 1).toUpperCase() +
+            data[1].slice(1)}
+        </p>
+        <p>{'number1 + number2 = ' + (number1 + number2)}</p>
+        <p>The string length is {string.length}</p>
       </header>
     </div>
   );
